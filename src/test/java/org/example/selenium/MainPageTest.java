d99e8cc3b7c904647e33a3712af3025313ddb326
Dochkin-sergey <96723560+Dochkin-sergey@users.noreply.github.com>
2024-06-04 14:23:35 +0300

    add test

--- src/test/java/org/example/selenium/MainPageTest.java
+++ src/test/java/org/example/selenium/MainPageTest.java
@@ -1,12 +1,21 @@
 package org.example.selenium;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -32,13 +41,42 @@ public class MainPageTest {
 
     @Test
     public void search() {
-        String input = "test";
+        String input = "Selenium";
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys(input);
         searchField.submit();
 
-
         WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
         assertEquals(input, searchPageField.getAttribute("value"));
+
+
+    }
+
+    @Test
+    public void url() {
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
+        ));
+        List<WebElement> issuance = driver.findElements(By.cssSelector("h2 > a[href]"));
+        issuance.get(0).click();
+
+//       Не разобрался как сделать это через вспомогательный метод, делал все как в уроке, клик не происходит если пользуюсь вспомогательным методом
+//       Помогите плиз
+//       clickElement(issuance, 0);
+
+        ArrayList tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1).toString());
+        String URL = driver.getCurrentUrl();
+        assertEquals(URL, "https://www.selenium.dev/");
+
+    }
+
+    public void clickElement(List<WebElement> issuance, int num) {
     }
 }
